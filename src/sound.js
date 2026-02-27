96b59770330c493e757cec93acb7a4dad9ef7685
Igor Gregori <igsilvacarlos@gmail.com>
2026-02-27 20:28:58 -0300

    fix assets

--- src/sound.js
+++ src/sound.js
@@ -1,5 +1,5 @@
 export function playWaterDrip() {
-  const waterDripAudio = new Audio("src/sounds/water-drip.mp3");
+  const waterDripAudio = new Audio("water-drip.mp3");
   waterDripAudio.volume = 0.2;
   waterDripAudio.play();
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,5 @@
 import { defineConfig } from "vite";
 
-export default defineConfig(() => {
-  return {
-    base: "/gradient-wallpaper-generator/",
-    server: {
-      port: 3000,
-      host: "0.0.0.0",
-    },
-  };
+export default defineConfig({
+  base: "/gradient-wallpaper-generator/",
 });
